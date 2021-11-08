7a217d684ad48f7d4d347ebc9b81d37880af64d9
Joshua Hartmann <45891115+joshuahartmann@users.noreply.github.com>
2021-11-08 00:50:25 -1000

    Set up boilerplate.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+ const express = require("express");
+ const cors = require("cors");
+ const helmet = require("helmet");
+ const { clientOrigins, serverPort } = require("./config/env.dev");
+ 
+ 
+ const app = express();
+ const apiRouter = express.Router();
+ 
+ app.use(helmet());
+ app.use(cors({ origin: clientOrigins }));
+ app.use(express.json());
+ 
+ app.use("/api", apiRouter);
+ 
+ apiRouter.use("/messages", messagesRouter);
+ 
+ app.use((err, req, res, next) => {
+   console.log(err);
+   res.status(500).send(err.message);
+ });
+ 
+ app.listen(serverPort, () =>
+   console.log(`API Server listening on port ${serverPort}`)
+ );
