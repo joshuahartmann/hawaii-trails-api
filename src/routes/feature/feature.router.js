862976df478a843db840ad9c1e142f17e48a9b6e
Jan Iverson Eligio <jeligio808@gmail.com>
2021-11-14 00:52:41 -1000

    Output proper geojson format in features route.

--- src/routes/feature/feature.router.js
+++ src/routes/feature/feature.router.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const { checkJwt } = require('../../authz/check-jwt');
 const { FeatureModel } = require('../../mongoose/models/Feature');
-
 const featureRouter = express.Router();
 
 featureRouter.get('/', (req, res) => {
-    FeatureModel.find()
+    FeatureModel.find({})
         .then((features) => {
-            res.send({ type: 'FeatureCollection', features });
+            const formattedFeatures = features.map((feature) => {
+                return {
+                    type: 'Feature',
+                    geometry: feature.geometry,
+                    properties: {
+                        name: feature.name,
+                        traffic: feature.traffic,
+                    },
+                };
+            });
+
+            res.send({
+                type: 'FeatureCollection',
+                features: formattedFeatures,
+            });
         })
         .catch((err) => {
+            console.log(err);
             res.status(500).send(err);
         });
 });
