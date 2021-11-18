df9afa504298f25bf24ac91b5206d0b9eb841750
Joshua Hartmann <45891115+joshuahartmann@users.noreply.github.com>
2021-11-18 04:23:36 -1000

    Implement function to check for point near line.

--- src/routes/checkins/checkins.service.js
+++ src/routes/checkins/checkins.service.js
@@ -13,21 +13,21 @@ const postCheckin = async (user, trail) => {
 };
 
 const createCheckin = async (userId, featureId, date) => {
-    await CheckinsModel.create({ userId, featureId, date });
+    await CheckInModel.create({ userId, featureId, date });
     return {
         message: 'Successfully checked in to a trail.',
     };
 };
 
 const queryRange = async (start, end) => {
-    const ret = await CheckinsModel.find({ date: { $gt: start, $lt: end } });
+    const ret = await CheckInModel.find({ date: { $gt: start, $lt: end } });
     return {
         message: 'Successfully queried check ins.',
         data: ret,
     };
 };
 
-const pointInRange = (polygon, point) => {
+const pointInPolygon = (polygon, point) => {
     let ret = false;
     const points = polygon.coordinates[0];
 
@@ -45,9 +45,46 @@ const pointInRange = (polygon, point) => {
     return ret;
 };
 
+const distance = (a, b) => {
+    let p1 = b[0] - a[0];
+    let p2 = b[1] - a[1];
+    let distance = 0;
+
+    p1 = p1 * p1;
+    p2 = p2 * p2;
+
+    distance = Math.sqrt(p1 + p2);
+
+    if (distance < 0) {
+        distance = distance * -1;
+    }
+
+    return distance;
+};
+
+const pointNearLine = (geometry, point) => {
+    let ret = false;
+    const points = geometry.coordinates;
+
+    if (geometry.type === 'LineString') {
+        // if (classifyPoint(points[i], point) === 0) {
+        //     ret = true;
+        // }
+        for (let i = 0; i < points.length; i++) {
+            // checks if approx. 36ft away from trail
+            if (distance(point, points[i]) <= 0.000109) {
+                ret = true;
+            }
+        }
+    }
+
+    return ret;
+};
+
 module.exports = {
     postCheckin,
     createCheckin,
     queryRange,
-    pointInRange,
+    pointInPolygon,
+    pointNearLine,
 };
