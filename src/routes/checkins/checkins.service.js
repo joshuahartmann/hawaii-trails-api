fbcc7246703567bd743f7462fcaa3ea4428a0b7c
Joshua Hartmann <45891115+joshuahartmann@users.noreply.github.com>
2021-11-18 03:08:37 -1000

    Implement functions for check in and point in range.

--- src/routes/checkins/checkins.service.js
+++ src/routes/checkins/checkins.service.js
@@ -3,6 +3,8 @@ const {
     CheckInModel,
 } = require('../../mongoose/models/CheckIn');
 
+const classifyPoint = require('robust-point-in-polygon');
+
 const postCheckin = async (user, trail) => {
     await CheckinsModel.create({ email: user.email, trail });
     return {
@@ -10,6 +12,42 @@ const postCheckin = async (user, trail) => {
     };
 };
 
+const createCheckin = async (userId, featureId, date) => {
+    await CheckinsModel.create({ userId, featureId, date });
+    return {
+        message: 'Successfully checked in to a trail.',
+    };
+};
+
+const queryRange = async (start, end) => {
+    const ret = await CheckinsModel.find({ date: { $gt: start, $lt: end } });
+    return {
+        message: 'Successfully queried check ins.',
+        data: ret,
+    };
+};
+
+const pointInRange = (polygon, point) => {
+    let ret = false;
+    const points = polygon.coordinates[0];
+
+    if (polygon.type === 'MultiPolygon') {
+        for (let i = 0; i < points.length; i++) {
+            if (ret === false) {
+                let inside = classifyPoint(points[i], point);
+
+                if (inside === 0 || inside === -1) {
+                    ret = true;
+                }
+            }
+        }
+    }
+    return ret;
+};
+
 module.exports = {
     postCheckin,
+    createCheckin,
+    queryRange,
+    pointInRange,
 };
