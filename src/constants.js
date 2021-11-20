cda7cc086be6cbb4d2f1b56cc21f7cef2be80ac1
Jan Iverson Eligio <jeligio808@gmail.com>
2021-11-19 14:08:51 -1000

    Create constants file.

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    OCTOBER_START_DATE: new Date(2021, 9, 1, 7, 0, 0),
+    OCTOBER_END_DATE: new Date(2021, 9, 31, 18, 0, 0),
+    NOVEMBER_END_DATE: new Date(2021, 10, 1, 18, 0, 0),
+    NOVEMBER_END_DATE: new Date(2021, 10, 30, 18, 0, 0),
+};
